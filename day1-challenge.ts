ed3f1a2cc795b057f328ede3d6df04e5037090a2
MuhammadSalman06 <114641797+MuhammadSalman06@users.noreply.github.com>
2024-03-03 21:39:57 +0500

    Day1 Challenge

---
+++ day1-challenge.ts
@@ -0,0 +1,10 @@
+let myname = "Salman Muccadam";
+
+console.log(`Hello ${myname}, would you like to learn some TypeScript today?`);
+
+
+let Name = "Salman Muccadam";
+
+console.log(Name.toLocaleLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase());
