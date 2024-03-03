4064c12ac50bf1185c8ca01ea45ea0e0620a3ba1
MuhammadSalman06 <114641797+MuhammadSalman06@users.noreply.github.com>
2024-03-03 21:41:50 +0500

    Day2 Challenge

---
+++ main.ts
@@ -0,0 +1,9 @@
+// console.log("Steve Jobs. The only way to do great work is to love what you do.");
+
+// let famousperson = "Steve Jobs";
+// let massege = "The only way to do great work is to love what you do.";
+// console.log(`${famousperson} ${massege}`);
+
+// let name1 =  "\t\n Steve Jobs \t\n";
+// console.log(name1);
+// console.log(name1.trim());
